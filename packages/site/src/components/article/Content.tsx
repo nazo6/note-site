2fa1614833da8821e1de51c823a2a0a7e4527e90
nazo6 <git@nazo6.dev>
2024-09-19 23:10:37 +0900

    use main tag instead of section tag

--- packages/site/src/components/article/Content.tsx
+++ packages/site/src/components/article/Content.tsx
@@ -10,7 +10,7 @@ export function ArticleContent({
   toc?: HeadingsReference;
 }) {
   return (
-    <section className="flex flex-col md:flex-row-reverse md:items-start md:gap-3">
+    <main className="flex flex-col md:flex-row-reverse md:items-start md:gap-3">
       {toc && toc.length > 0 && (
         <div className="top-header flex-shrink-0 md:sticky md:mt-0 md:w-toc">
           {toc.length > 0 && (
@@ -23,6 +23,6 @@ export function ArticleContent({
       <div className="min-w-0 mt-3 flex-grow px-2 pb-2 leading-7 bg-content shadow-lg pt-2">
         {children}
       </div>
-    </section>
+    </main>
   );
 }
