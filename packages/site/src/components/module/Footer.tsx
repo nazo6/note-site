972fc78499e34e8098d7b170509bedab11e91ffe
nazo6 <git@nazo6.dev>
2024-09-28 14:10:16 +0900

    add zenn link

--- packages/site/src/components/module/Footer.tsx
+++ packages/site/src/components/module/Footer.tsx
@@ -1,11 +1,15 @@
 import { SiGithub } from "@icons-pack/react-simple-icons";
+import { ZennIcon } from "../ui/ZennIcon";
 
 export function Footer() {
   return (
     <footer className="mx-auto w-full drop-shadow-[0_-5px_5px_rgba(0,0,0,0.1)]">
       <div className="flex w-full bg-content py-4 justify-center">
-        <span className="mr-2">©</span>nazo6
-        <div className="flex ml-5">
+        <span className="mr-2">©</span>
+        <a target="_blank" rel="noopener noreferrer" href="https://nazo6.dev">
+          nazo6.dev
+        </a>
+        <div className="flex ml-5 gap-2">
           <a
             href="https://github.com/nazo6"
             target="_blank"
@@ -13,6 +17,14 @@ export function Footer() {
             aria-label="nazo6 github"
           >
             <SiGithub />
+          </a>
+          <a
+            href="https://zenn.dev/nazo6"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="nazo6 zenn"
+          >
+            <ZennIcon />
           </a>
         </div>
       </div>
