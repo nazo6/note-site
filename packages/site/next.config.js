fb8451ff4e322c5bff30bc01594da5db33da7b11
nazo6 <git@nazo6.dev>
2024-09-30 17:46:27 +0900

    add bundle analyzer

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -7,4 +7,7 @@ const nextConfig = {
   output: "export",
 };
 
-module.exports = nextConfig;
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+module.exports = withBundleAnalyzer(nextConfig);
